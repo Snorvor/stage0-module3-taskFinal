1c80bf2362fde0c72b8057d301e91d22ab237f21
Snorvor <bulezar@gmail.com>
2025-07-19 19:03:28 -0300

    original number reverted

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,26 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        // (489 % 10) = 9
+        // 9 * 100 = 900
+        int revertedNumber = (number % 10) * 100;
+
+        // (489 - 489 % 10) = 480
+        // / 10 = 48
+        number = (number - number % 10) / 10;
+
+        // (number % 10) = 8
+        // * 10 = 80;
+        // revertedNumber + ... = 980
+        revertedNumber = revertedNumber + (number % 10) * 10;
+
+        // (48 - 48 % 10) = 40
+        // / 10 = 4
+        number = (number - number % 10) / 10;
+
+        // 4 + 980 = 984
+        revertedNumber = revertedNumber + number;
+
+        System.out.println(revertedNumber);
     }
 }
