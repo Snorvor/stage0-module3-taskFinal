bf2ec9e12073538ee6fa33cd538f9567d2206491
Snorvor <bulezar@gmail.com>
2025-07-20 17:14:15 -0300

    celsius to fahrenheit converted

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        float temperatureFahrenheit = (temperatureCelsius * 1.8F) + 32;
+
+        System.out.println(temperatureFahrenheit);
     }
 }
