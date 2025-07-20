07f726e0676f1918e63e41875c9a3b2fcbf9c5f2
Snorvor <bulezar@gmail.com>
2025-07-20 16:59:10 -0300

    found the sum of the digits

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,31 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int firstNumber, secondNumber, thirdNumber, fourthNumber, sumOfEachValue;
+
+        // to obtain fourth number
+        fourthNumber = number % 10;
+
+        // to eliminate fourth number of the original number
+        number = number / 10;
+
+        // to obtain third number
+        thirdNumber = number % 10;
+
+        // to eliminate third number of the original number
+        number = number / 10;
+
+        // to obtain second number
+        secondNumber = number % 10;
+
+        // to eliminate second number of the original number
+        number = number / 10;
+
+        // the first digit of var number is assigned to firstNumber
+        firstNumber = number;
+
+        sumOfEachValue = firstNumber + secondNumber + thirdNumber + fourthNumber;
+
+        System.out.println(sumOfEachValue);
     }
 }
